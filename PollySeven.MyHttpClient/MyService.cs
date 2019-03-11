d92cd08c4652fe04b6c602eadd8b3fdf721f3c48
Aliaksandr Tsoi <aliaksandr.tsoi@aqr.com>
2019-03-11 18:26:17 -0400

    Add initial sample

---
+++ PollySeven.MyHttpClient/MyService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Http;
+using Polly;
+
+namespace PollySeven.MyHttpClient
+{
+    public class MyService
+    {
+        private readonly HttpClient _httpClient;
+
+        public MyService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+
+            // emulate an explicit dependency on Polly
+            var policy = Policy.Handle<Exception>().RetryAsync();
+        }
+    }
+}
---
+++ PollySeven/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PollySeven.MyHttpClient;
+
+namespace PollySeven
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var services = new ServiceCollection();
+            services.AddTransient<MyService>();
+
+            ServiceProvider provider = services.BuildServiceProvider(true);
+
+            var service = provider.GetRequiredService<MyService>();
+
+            // System.InvalidOperationException:
+            // 'Unable to resolve service for type 'System.Net.Http.HttpClient' while attempting to activate 'PollySeven.MyHttpClient.MyService'.'
+        }
+    }
+}
