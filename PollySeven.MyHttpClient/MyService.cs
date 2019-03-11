2e9ab80be64f0ee4d8d8e2c255fd4fd39aad6fa3
Aliaksandr Tsoi <aliaksandr.tsoi@aqr.com>
2019-03-11 18:29:10 -0400

    Update sample to throw TypeLoadException

--- PollySeven.MyHttpClient/MyService.cs
+++ PollySeven.MyHttpClient/MyService.cs
@@ -1,17 +1,12 @@
 ﻿using System;
-using System.Net.Http;
 using Polly;
 
 namespace PollySeven.MyHttpClient
 {
     public class MyService
     {
-        private readonly HttpClient _httpClient;
-
-        public MyService(HttpClient httpClient)
+        public MyService()
         {
-            _httpClient = httpClient;
-
             // emulate an explicit dependency on Polly
             var policy = Policy.Handle<Exception>().RetryAsync();
         }
--- PollySeven/Program.cs
+++ PollySeven/Program.cs
@@ -14,8 +14,8 @@ namespace PollySeven
 
             var service = provider.GetRequiredService<MyService>();
 
-            // System.InvalidOperationException:
-            // 'Unable to resolve service for type 'System.Net.Http.HttpClient' while attempting to activate 'PollySeven.MyHttpClient.MyService'.'
+            // System.TypeLoadException:
+            // 'Could not load type 'Polly.RetrySyntaxAsync' from assembly 'Polly, Version=7.0.0.0, Culture=neutral, PublicKeyToken=c8a3ffc3f8f825cc'.'
         }
     }
 }
